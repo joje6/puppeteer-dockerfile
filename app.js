aeff9578021ba8e4b8e52c7508780300077e3451
KYEONGMIN CHO <ggingmin@gmail.com>
2024-05-02 13:48:40 +0900

    commit

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const puppeteer = require('puppeteer');
+const AWS = require('aws-sdk');
+const fs = require('fs');
+
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -11,9 +14,31 @@ const puppeteer = require('puppeteer');
     ],
     ignoreHTTPSErrors: true,
   });
+
   const page = await browser.newPage();
-  await page.goto('https://cloudtype.io');
-  await page.screenshot({ path: 'cloudtype.png' });
+  await page.goto(process.env.PAGE_GOTO);
+
+  const screenshotPath = 'screenshot.png';
+  await page.screenshot({ path: screenshotPath });
+
+  const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY
+  });
+
+  const uploadParams = {
+    Bucket: process.env.AWS_S3_BUCKET,
+    Key: process.env.AWS_S3_FILENAME,
+    Body: fs.createReadStream(screenshotPath)
+  };
+
+  s3.upload(uploadParams, function(err, data) {
+    if (err) {
+      console.error("에러 발생:", err);
+    } else {
+      console.log("업로드 성공:", data.Location);
+    }
+  });
 
   await new Promise(resolve => setTimeout(resolve, 600000)); 
 
