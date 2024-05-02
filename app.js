a4863ed9c1ba2ffc02bca536447260f5a6e33133
KYEONGMIN CHO <ggingmin@gmail.com>
2024-05-02 14:09:01 +0900

    commit

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const fs = require('fs');
   const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_KEY,
-    endpoint: AWS_S3_ENDPOINT_URL,
+    endpoint: process.env.AWS_S3_ENDPOINT_URL,
   });
 
   const uploadParams = {
