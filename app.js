7338d6e65b1fb62ac4e4050b0f9588cda697eff6
KYEONGMIN CHO <ggingmin@gmail.com>
2024-05-02 11:58:06 +0900

    commit

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    executablePath: `/usr/bin/chromium-browser`,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
