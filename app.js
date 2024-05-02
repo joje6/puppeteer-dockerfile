7b368dba44d876b863353e9b786b7f881b16eac2
KYEONGMIN CHO <ggingmin@gmail.com>
2024-05-02 11:51:48 +0900

    commit

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: "new",
+    headless: true,
+    executablePath: `/usr/bin/google-chrome`,
     args: [
       '--no-sandbox',
-      '--disable-dev-shm-usage'
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
     ],
+    ignoreHTTPSErrors: true,
   });
   const page = await browser.newPage();
   await page.goto('https://cloudtype.io');
