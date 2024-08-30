bd88027d8d4bce3520e41b83aa01ec5cf4b0e220
joje <joje.attrs@gmail.com>
2024-08-30 10:19:34 +0900

    ...

--- app.js
+++ app.js
@@ -2,52 +2,36 @@ const puppeteer = require("puppeteer");
 const { v4: uuidv4 } = require("uuid");
 const express = require("express");
 
-const fs = require("fs");
-const path = require("path");
-
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next) => {
-  if (req.url.endsWith('.css')) {
-      res.type('text/css');
-  }
-  next();
-});
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index", { pageUrl: "" });
+  res.send("OK");
 });
 
 app.get("/test", async (req, res) => {
-  const { pageUrl } = req.body;
+  try {
+    browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-gpu",
+      ],
+      ignoreDefaultArgs: ["--disable-extensions"],
+      ignoreHTTPSErrors: true,
+    });
 
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-gpu",
-    ],
-    ignoreHTTPSErrors: true,
-  });
-
-  const page = await browser.newPage();
-  await page.goto(pageUrl, { waitUntil: "networkidle0", timeout: 120000 });
-  await page.setViewport({ width: 1920, height: 1024 });
-
-  const screenshotPath = "screenshot.png";
-  await page.screenshot({ path: screenshotPath });
-
-  await browser.close();
-
-  res.send({src:screenshotPath});
+    const page = await browser.newPage();
+    page.once("load", () => res.send({ loaded: true }));
+    page.on("error", (err) => res.send({ error: true }));
+    await page.goto('https://google.com');
+  } catch (err) {
+    res.send({ error: true, message: err.message });
+  }
 });
 
 function generateRandomFilename() {
